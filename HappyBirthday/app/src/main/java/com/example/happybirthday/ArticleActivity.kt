08768cea64d56552f482d88ceba52c4fc9048031
rt-karthi <rtkarthi27@gmail.com>
2024-10-16 12:30:18 +0530

    Compose Article Completed

--- HappyBirthday/app/src/main/java/com/example/happybirthday/ArticleActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/ArticleActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -27,13 +30,11 @@ class ArticleActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ArticleJetpack(
-                        title = stringResource(R.string.title),
-                        paragraphOne = stringResource(R.string.paragraph_one),
-                        paragraphTwo = stringResource(R.string.paragraph_two),
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticle()
                 }
             }
         }
@@ -41,28 +42,35 @@ class ArticleActivity : ComponentActivity() {
 }
 
 @Composable
-fun ArticleJetpack(title: String, paragraphOne: String, paragraphTwo: String,modifier: Modifier = Modifier) {
-    Column(modifier) {
-        val image = painterResource(R.drawable.bg_compose_background)
+fun ComposeArticle() {
+    ArticleJetpack(
+        title = stringResource(R.string.title),
+        paragraphOne = stringResource(R.string.paragraph_one),
+        paragraphTwo = stringResource(R.string.paragraph_two),
+        imagepainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
 
+@Composable
+private fun ArticleJetpack(title: String, paragraphOne: String, paragraphTwo: String, imagepainter: Painter,modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
         Image(
-            painter = image,
+            painter = imagepainter,
             contentDescription = null
         )
         Text(
             text = title,
             fontSize = 24.sp,
-            modifier = Modifier
-                .padding(16.dp)
+            modifier = Modifier.padding(16.dp)
         )
         Text(
             text = paragraphOne,
-            fontSize = 16.sp,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
             textAlign = TextAlign.Justify
         )
         Text(
             text = paragraphTwo,
-            fontSize = 16.sp,
+            modifier = Modifier.padding(16.dp),
             textAlign = TextAlign.Justify
         )
 
@@ -73,10 +81,6 @@ fun ArticleJetpack(title: String, paragraphOne: String, paragraphTwo: String,mod
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        ArticleJetpack(
-            title = stringResource(R.string.title),
-            paragraphOne = stringResource(R.string.paragraph_one),
-            paragraphTwo = stringResource(R.string.paragraph_two)
-        )
+        ComposeArticle()
     }
 }
